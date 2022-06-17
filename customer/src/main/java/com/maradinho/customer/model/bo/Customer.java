3c4bd42d48767ddb693c9ddc3cd0f506afffdbac
Maradinho10 <edder_7@hotmail.com>
2022-06-16 19:45:47 -0500

    App start up problems solved.

--- customer/src/main/java/com/maradinho/customer/model/bo/Customer.java
+++ customer/src/main/java/com/maradinho/customer/model/bo/Customer.java
@@ -21,7 +21,10 @@ public class Customer {
     @SequenceGenerator(name = "customer_sequence", sequenceName = "customer_sequence")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_sequence")
     private Integer id;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
 }
--- fraud/src/main/java/com/maradinho/fraud/model/bo/FraudCheckHistory.java
+++ fraud/src/main/java/com/maradinho/fraud/model/bo/FraudCheckHistory.java
@@ -23,13 +23,18 @@ public class FraudCheckHistory {
             sequenceName = "fraud_id_sequence",
             name = "fraud_id_sequence"
     )
+
     @GeneratedValue(
         strategy = GenerationType.SEQUENCE,
         generator = "fraud_id_sequence"
 
     )
+
     private Integer id;
+
     private Integer customerId;
+
     private Boolean isFraudster;
+
     private LocalDateTime createdAt;
 }
